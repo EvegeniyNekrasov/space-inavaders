2b816912e7c4d79607f72ce66b7e820b72a3241f
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2024-02-13 19:39:24 +0100

    adding game loop and espace key event

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,11 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
-use std::error::Error;
+use std::{error::Error, io, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -11,6 +17,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "win.wav");
 
     audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {} // ignore every thing else
+                }
+            }
+        }
+    }
+
+    // Clean up
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
